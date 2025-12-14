d851dda53accafb50c984207c6502951641267d4
Kaj Lund <kaj.lund@gmail.com>
2025-12-14 09:57:24 +0200

    Adds better styling, feathers icons and dark mode toggle

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,18 @@
-console.log('app.js loaded');
+const DARKMODEKEY = 'Activitz.darkMode';
+
+// Check if darkmode is enabled and set class
+const darkMode = localStorage.getItem(DARKMODEKEY, '');
+if (darkMode) {
+  document.body.classList.add('dark-mode');
+}
+
+// Add event listener to theme toggle button and store preference
+const btnThemeToggle = document.querySelector('#theme-toggle-btn');
+btnThemeToggle.addEventListener('click', () => {
+  document.body.classList.toggle('dark-mode');
+  if (document.body.classList.contains('dark-mode')) {
+    localStorage.setItem(DARKMODEKEY, 'Y');
+  } else {
+    localStorage.removeItem(DARKMODEKEY);
+  }
+});
